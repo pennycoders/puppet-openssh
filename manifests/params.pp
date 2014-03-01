41e550419e1c7933d8681ea5d7ccf75ce8520cf8
Alex Pica <dtk077@gmail.com>
2014-03-01 03:10:42 +0200

    Update classes

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,24 @@
 #
 # Do not call directly.
 #
-class openssh::params {
+class openssh::params (
+  $port                    = 22,
+  $permit_root_login       = 'no',
+  $log_level               = 'INFO',
+  $x11_forwarding          = 'no',
+  $max_auth_tries          = 4,
+  $password_authentication = 'yes',
+  $ciphers                 = 'aes128-ctr,aes192-ctr,aes256-ctr',
+  $client_alive_interval   = 300,
+  $client_alive_count_max  = 0,
+  $allow_users             = 'UNSET',
+  $deny_users              = 'UNSET',
+  $banner                  = '/etc/issue.net',
+  $sftp_chroot             = false,
+  $service_ensure          = running,
+  $service_enable          = true,
+  $package_name            = 'openssh-server',
+  $package_ensure          = present) {
   case $::osfamily {
     redhat  : {
       $service_name    = 'sshd'
